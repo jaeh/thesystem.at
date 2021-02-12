4240e8f7b8859aa76dc5b994354f60c11b4f3568
jaeh <jascha@jaeh.at>
2021-02-13 00:59:39 +0100

    Team: update styles for new layout

--- src/assets/Team.mjs
+++ src/assets/Team.mjs
@@ -1,36 +1,39 @@
 export const View = team =>
-  ul(team.map(member => li([h3(a({ href: member.url }, member.name)), member.job.map(j => p(j))])))
+  ul(
+    { class: 'Team' },
+    team.map(member => li([h3(a({ href: member.url }, member.name)), member.job.map(j => p(j))])),
+  )
 
 export const style = v => ({
   display: 'inline-block',
 
   li: {
     border: '1px solid',
-    height: '40vw',
+    height: '50vw',
     margin: '0 2vw 2vw 0',
     maxHeight: '200px',
     maxWidth: '200px',
     textAlign: 'center',
-    width: '40vw',
+    width: '50vw',
 
-    h5: {
-      fontSize: '14px',
+    h3: {
+      fontSize: '20px',
       lineHeight: '25px',
       margin: '0.2em 0',
     },
 
     p: {
-      fontSize: '12px',
+      fontSize: '15px',
       lineHeight: '1.3em',
       margin: 0,
     },
   },
 
-  [`@media screen and (min-width: ${v.widths.tablet})`]: {
+  [`@media screen and (min-width: ${v.widths.laptop})`]: {
     li: {
       float: 'left',
-      width: '27vw',
-      height: '27vw',
+      width: '23vw',
+      height: '23vw',
     },
   },
 })
