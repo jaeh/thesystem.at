e7bf2cd27a62475a5b6997897cdda8a6296635a3
jaeh <jascha@jaeh.at>
2021-02-14 18:36:20 +0100

    Text maximum width of 900px

--- src/assets/Text.mjs
+++ src/assets/Text.mjs
@@ -3,7 +3,7 @@ export const View = children => div({ class: 'Text' }, children)
 export const style = v => ({
   clear: 'both',
   padding: '0 1em 0',
-  margin: '0 auto',
+  maxWidth: '900px',
 
   [`@media screen and (min-width: ${v.widths.laptop})`]: {
     padding: 0,
