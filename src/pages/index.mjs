6e98e81af9619b4d3756492124b3b1599e4e4f07
jaeh <jascha@jaeh.at>
2021-02-14 20:09:14 +0100

    pages: update contents

--- src/pages/index.mjs
+++ src/pages/index.mjs
@@ -17,7 +17,8 @@ export const View = ({ artists, collectives, team }) => [
         ' collectively using art and technology to transform our world.',
       ]),
       p([
-        'We believe in a literal definition of “common goods”',
+        'We believe in a literal definition of ',
+        Quote('common goods'),
         ' and transform public space into a bearer of art,',
         ' turning those works of art into an integral part of urban culture.',
       ]),
@@ -33,29 +34,29 @@ export const View = ({ artists, collectives, team }) => [
   }),
 
   Content([
-    Slogan('We are timeless, have unlimited space and are always open to the public.'),
+    Slogan(['We are timeless,', ' have unlimited space and are always open to the public.']),
 
     Text([
       p([
         'The cultural mission of ',
         i('TheSystemCollective'),
-        ' is the creation, procurement, research, dissemination and conservation of (digital) art for the ',
-        b(Link({ to: 'https://artificialmuseum.com/', text: 'Artificial Museum' })),
-        ', a time space continuum made ',
-        b('manifest'),
-        ', created to make art accessible to everyone, both at home and abroad.',
+        ' is the creation, procurement, research, dissemination and conservation of',
+        ' (digital) art for the ',
+        Link({ text: 'Artificial Museum', to: 'https://artificialmuseum.com' }),
+        ' a time space continuum made manifest, created',
+        ' to make art accessible to everyone, both at home and abroad.',
       ]),
-
       p([
-        'Our method is the intermixture of virtual, imaginary worlds with reality,',
+        'Our method is the intermixture of virtual worlds with reality,',
         ' allowing the free placement of artworks in public spaces.',
       ]),
 
-      p(
-        Quote(
-          'Art is not the way to more democracy and equality, but art can be the impetus for a dialog.',
-        ),
-      ),
+      p([
+        Cite({
+          quote: 'Art cannot save people, but the means of art can make a dialogue possible.',
+          author: 'Günther Uecker',
+        }),
+      ]),
     ]),
   ]),
 
@@ -67,20 +68,25 @@ export const View = ({ artists, collectives, team }) => [
   }),
 
   Content([
-    Slogan("Don't ask, just do."),
+    Slogan("Don't ask, just act."),
 
     Text([
       p([
-        'The first exhibition in the ARM: ',
-        Quote('TheSystem - Phase.1 - 2020'),
-        ' took place at the Yppenplatz in Vienna',
-        ' and laid the foundation stone for the Artificial Museum.',
+        'The first exhibition in the ARM: “',
+        Link({ to: '/phase1/', text: 'TheSystem - Phase.1 - 2020' }),
+        '” took place at the Yppenplatz in Vienna and laid the foundation stone for the Artificial Museum.',
       ]),
 
       p([
-        'Our long term responsibility is to expand the ARM,',
-        ' offer young artists around the world unlimited space to make their art,',
-        ' and make the results of our work accessible to a broad public.',
+        'Our long term responsibility is to scale the walls of',
+        ' institutionalized art exhibits and galleries,',
+        ' using public spaces all around the world as exhibition grounds,',
+        ' turning well known as well as almost forgotten places into stages for living artists.',
+      ]),
+
+      p([
+        'The resulting works are to be exhibited in open virtual space with',
+        ' expanded optimism and resilience facing the new era.',
       ]),
 
       p(
@@ -105,19 +111,23 @@ export const View = ({ artists, collectives, team }) => [
   }),
 
   Content([
-    Slogan('Agenda 2021'),
+    Slogan('No Walls for …'),
 
     Text([
-      h3('February: Opening'),
-      p('We opened the Artificial Museum on the 1st of February.'),
-
-      h3('May - November: No Walls'),
       p([
-        'The exhibition series "No Walls" aims',
-        ' to create a decentralized but localized exhibition all over Vienna and other cities.',
+        'Manifesting ourselves in the form of artistic interventions and expanded exhibitions',
+        ' in public space, we focus on making art accessible to all inhabitants',
+        ' and social groups in and beyond the city of Vienna.',
       ]),
 
-      Link({ to: '/nowalls/', text: 'More about No Walls' }),
+      h3('No walls, but open imagination.'),
+
+      p([
+        'An encouragement for new experimental creations, or 3D re-creations,',
+        ' to be exhibited within the frame of sharing practices and radical philosophies.',
+      ]),
+
+      p([Link({ to: '/nowalls/', text: 'More about No Walls' })]),
     ]),
   ]),
 
--- src/pages/nowalls.mjs
+++ src/pages/nowalls.mjs
@@ -4,35 +4,54 @@ export const state = {
 
 export const View = () => [
   Content([
-    Slogan('No Walls: 2021'),
+    Slogan([
+      'No walls for …',
+      br(),
+      'Artists,',
+      br(),
+      'Education,',
+      br(),
+      'Science',
+      br(),
+      'and',
+      br(),
+      'Museums',
+    ]),
 
     Text([
       p([
-        'The exhibition series ',
-        Quote('No Walls for ...'),
-        ' creates decentralized, localized art experiences all over Vienna and beyond.',
-      ]),
-      p([
-        'This includes multiple exhibitions, interventions and collaborations',
-        ' which will happen throughout 2021.',
-      ]),
-      p([
-        'We will put our focus on all areas of life in which our freedoms are restricted,',
-        ' on retracing of power structures and on dissolving them.',
+        'The exhibition series “No Walls for ...” creates',
+        ' decentralized, localized art experiences all over Vienna and beyond.',
+
+        p([
+          'We rely on the active participation of the urban population,',
+          ' turning public spaces into an art exhibition housing independent and contemporary art.'
+        ]),
+
+        p([
+          i('TheSystemCollective'),
+          ' will cooperate with artists, collectives, museums and institutions,',
+          ' to conceptualize and implement diverse art practices as 3d experiences.',
+        ]),
+        p('Each created artwork will become a part of public space.'),
+
+        Quote('Boundaries lead to the decline of art and culture. The profit motive establishes and reinforces boundaries.'),
       ]),
     ]),
   ]),
-
-  Content(VideoEmbed({ src: 'D6i50aYe-X8' })),
 
   Content([
     Slogan('No Walls For Artists'),
 
     Text([
       p([
-        'Starting on the 1st of May 2021,',
-        ' we will feature artists and their artworks.',
-        ' Featured artworks can be positioned worldwide.',
+        'The Artificial Museum features chosen artists for a period of one month.',
+        ' Starting on the 1st of May 2021, each featured work can be positioned all around the world.',
+      ]),
+      p([
+        'First featured artist: ',
+        Link({ text: 'Barbis Ruder', to: 'https://barbisruder.com' }),
+        ' more details coming soon.',
       ]),
     ]),
   ]),
@@ -42,8 +61,8 @@ export const View = () => [
 
     Text([
       p([
-        'We will collaborate with institutions, exhibitions and collectives,',
-        ' working together to represent our shared visions.',
+        'We will cooperate with universities in Vienna, organizing workshops and specific expos,',
+        ' as well as creating collaborative concepts with classes from the participating institutions.',
       ]),
     ]),
   ]),
@@ -53,10 +72,15 @@ export const View = () => [
 
     Text([
       p([
-        'Beginning on October 1st ("Lange Nacht der Museen", "Tag des Museums")',
-        ' and ongoing until November 9th ("Visit an Art Museum day"),',
-        ' "No Walls for Museums" will position artworks around austrian museums and close to art exhibitions, institutions and other art and culture related places.',
-        // ' These artworks will be a permanent commemoration to those important places of culture and history.',
+        'Using the front doors of Museums to showcase conceptual art pieces',
+        " that represent the museum's art collection.",
+      ]),
+      p([
+        'Beginning on October 1st ("Lange Nacht der Museen", "Tag des Museums") and',
+        ' ongoing until November 9th ("Visit an Art Museum day"), ',
+        Quote('No Walls for Museums'),
+        ' will position artworks around museums and close to',
+        ' art exhibitions, institutions and other places of art and culture.',
       ]),
     ]),
   ]),
